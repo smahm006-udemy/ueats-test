9e7a8088c02c6e00996f5e2d1873fbf252c5183d
Sohaib Mahmood <soh.mahmood@fastmail.com>
2025-01-26 21:32:50 -0500

    feat: keeping only one database test

--- tests/test_database.py
+++ tests/test_database.py
@@ -7,6 +7,7 @@ ueats CLI database test
 
 import json
 from subprocess import check_output
+from .utils import run_and_check
 from pathlib import Path
 
 
@@ -19,26 +20,27 @@ def proccess_json_output(text: str):
 
 
 def test_database_setup():
-    db_setup_res = check_output(f"ueats database setup", shell=True, text=True)
+    db_setup_res = check_output(["ueats", "database", "setup"], text=True)
     status, output, error = proccess_json_output(db_setup_res)
-    assert status == 0 and not error, f"Got error setting up database\nError: {error}"
-    assert "successfully" in output, "Output did not contain 'successfully' string"
+    assert status == 0, f"Got an unexpected status - {status}"
+    assert not error, f"Got error setting up database - {error}"
+    assert "Tables created successfully" in output, "Output did not contain 'successfully' string"
     ueats_file = Path.cwd() / "ueats.db"
-    assert ueats_file.exists()
-    db_list_res = check_output(f"ueats database list", shell=True, text=True)
+    assert ueats_file.exists(), ""
+    db_list_res = check_output(["ueats", "database", "list"], text=True)
     status, output, error = proccess_json_output(db_list_res)
     assert status == 0 and not error, f"Got error listing database\nError: {error}"
     output_tables = list(output.keys())
     expected_tables = ["users", "restaurants", "menus", "orders", "deliveries"]
-    assert all(x in output_tables for x in expected_tables)
+    for table in output_tables:
+        assert table in expected_tables
 
 def test_database_teardown():
-    db_teardown_res = check_output(f"ueats database teardown", shell=True, text=True)
-    status, _, error = proccess_json_output(db_teardown_res)
-    assert status == 0 and not error, f"Got error setting up database\nError: {error}"
+    # Check command runs successfully
+    run_and_check("ueats database teardown")
+    # Check expected file still exists
     ueats_file = Path.cwd() / "ueats.db"
     assert ueats_file.exists()
-    db_list_res = check_output(f"ueats database list", shell=True, text=True)
-    status, output, error = proccess_json_output(db_list_res)
-    assert status == 0 and not error, f"Got error listing database\nError: {error}"
+    # Verify tables are empty database list
+    output = run_and_check("ueats database list")
     assert all(not x for x in output.values())
