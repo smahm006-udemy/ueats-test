346e41db76dfca9937c1693760919707652e8787
Sohaib Mahmood <soh.mahmood@fastmail.com>
2025-02-01 15:17:38 -0500

    feat: changing custom logger to be a class

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -7,75 +7,47 @@ Basic logging structure
 
 import pytest
 import logging
-from enum import Enum
 
 
-# Define LogLevel Enum
-class LogLevel(Enum):
-    DEBUG = logging.DEBUG
-    INFO = logging.INFO
-    WARNING = logging.WARNING
-    ERROR = logging.ERROR
-    CRITICAL = logging.CRITICAL
-    EMERGENCY = 60
+class CustomLogger(logging.Logger):
+    class CustomFormatter(logging.Formatter):
+        grey = "\x1b[38;20m"
+        yellow = "\x1b[33;20m"
+        red = "\x1b[31;20m"
+        bold_red = "\x1b[31;1m"
+        reset = "\x1b[0m"
+        format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+        FORMATS = {
+            logging.DEBUG: grey + format + reset,
+            logging.INFO: grey + format + reset,
+            logging.WARNING: yellow + format + reset,
+            logging.ERROR: red + format + reset,
+            logging.CRITICAL: bold_red + format + reset,
+        }
 
+        def format(self, record):
+            log_fmt = self.FORMATS.get(record.levelno)
+            formatter = logging.Formatter(log_fmt)
+            return formatter.format(record)
 
-# Register the custom log levels with logging module
-logging.addLevelName(LogLevel.EMERGENCY.value, "EMERGENCY")
-
-
-# Create custom emergency function tp use with logging.
-def emergency(self, message, *args, **kwargs):
-    if self.isEnabledFor(LogLevel.EMERGENCY.value):
-        self._log(LogLevel.EMERGENCY.value, message, args, **kwargs)
-
-
-# Attach function to Logger class
-logging.Logger.emergency = emergency
-
-
-def custom_logger(name: str, level: LogLevel) -> logging.Logger:
-    # Create a named logger
-    logger = logging.getLogger(name)
-    logger.setLevel(level.value)  # Set logger level using enum value
-
-    # Create a console handler and set its level
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(level.value)
-
-    # Create a file handler and set its level
-    file_handler = logging.FileHandler(f"{name}.log")
-    file_handler.setLevel(level.value)
-
-    # Set the formatter for the console and file handlers
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        datefmt="%m/%d/%Y %I:%M:%S%p",
-    )
-    console_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
-
-    # Add both handlers to the logger
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
-
-    return logger
+    def __init__(self, name, level=logging.DEBUG):
+        super().__init__(name, level)
+        handler = logging.StreamHandler()
+        handler.setFormatter(self.CustomFormatter())
+        self.addHandler(handler)
 
 
 @pytest.mark.logging
 class TestLogging:
-    # Example usage
-    logger = custom_logger("custom_logger", LogLevel.DEBUG)
-
     def test_default_logger(self):
-        logging.debug(f"Running inside class - {__class__}")
-        logging.info("This is a simple log message")
+        default_logger = logging.getLogger("default_logger")
+        default_logger.debug(f"Running inside class - {__class__}")
+        default_logger.info("This is a simple log message")
 
     def test_custom_logger(self):
-        # Logging messages with different levels
-        self.logger.debug("This is a DEBUG level message.")
-        self.logger.info("This is an INFO level message.")
-        self.logger.warning("This is a WARNING level message.")
-        self.logger.error("This is an ERROR level message.")
-        self.logger.critical("This is a CRITICAL level message.")
-        self.logger.emergency("This is an EMERGENCY level message.")
+        custom_logger = CustomLogger(name="custom_logger")
+        custom_logger.debug("This is a DEBUG level message.")
+        custom_logger.info("This is an INFO level message.")
+        custom_logger.warning("This is a WARNING level message.")
+        custom_logger.error("This is an ERROR level message.")
+        custom_logger.critical("This is a CRITICAL level message.")
